3182ce28efb0f41fbeff7b130a4d974411a28d10
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2024-07-14 23:01:18 +0500

    Add files via upload

---
+++ Q8.js
@@ -0,0 +1,7 @@
+// Write a function to check if a number is even or odd.
+
+function evenodd(num){
+    let num=5;
+    return alert("The number is "+ (num%2===0 ? "EVEN." : "ODD."))
+}
+evenodd(5);
