32c4bfcf839a1ed1d25b5508a130323ac165d235
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2024-07-14 23:00:54 +0500

    Add files via upload

---
+++ Q5.js
@@ -0,0 +1,7 @@
+// Find the index of an element in an array.
+function indexofele(arr,ele){
+    return alert("The index of "+ele+" is : "+arr.indexOf(ele));   
+}
+let arr=[5,8,1,2,9,0];
+let ele=2;
+indexofele(arr,ele);
