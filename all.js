7ce4afcb05aaa433ddff00b8092c294165dbf740
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2024-07-15 21:30:47 +0500

    all.js

--- all.js
+++ all.js
@@ -71,21 +71,37 @@ function evenodd(num){
 
 //Write a sync and Async funtion to fetch data from 3 APIs with delays 10000, 5000 and 7000 respectively.
 
-function asyncfetch() {
-    let promise1 = fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(response => response.json());
-    let promise2 = fetch('https://jsonplaceholder.typicode.com/todos/2')
-        .then(response => response.json());
-    let promise3 = fetch('https://jsonplaceholder.typicode.com/todos/3')
-        .then(response => response.json());
+async function fetchDataAsync() {
+    const fetchApi1 = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Data from API 1');
+        }, 10000);
+    });
 
-    return Promise.all([promise1, promise2, promise3])
-        .then((values) => {
-            console.log(values);
-            return values;
-        })
-        .catch((error) => {
-            console.error(error);
-            return [];
-        });
+    const fetchApi2 = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Data from API 2');
+        }, 5000);
+    });
+
+    const fetchApi3 = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Data from API 3');
+        }, 7000);
+    });
+
+    try {
+        const data1 = await fetchApi1;
+        console.log(data1);
+
+        const data2 = await fetchApi2;
+        console.log(data2);
+
+        const data3 = await fetchApi3;
+        console.log(data3);
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
+
+fetchDataAsync();
